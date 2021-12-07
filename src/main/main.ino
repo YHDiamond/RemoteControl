3636a0130de7ec42eeea72ebf63b350178611b12
Avraham Hollander <anhollander516@gmail.com>
2021-12-07 16:21:06 -0500

    Update files

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
 
+  // initialize digital pins for Bluetooth Tx and Rx
+  // initialize analog pins for joystick x and y
+
+  //Whatever code we need to use to interface with the Bluetooth module
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
