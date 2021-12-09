d44d0bfcfc4a6e1808d804177e3eb5e7ad8908a2
Avraham Hollander <anhollander516@gmail.com>
2021-12-09 14:43:58 -0500

    Add descriptive comments

--- src/main/main.ino
+++ src/main/main.ino
@@ -5,39 +5,35 @@
 #endif
 
 BluetoothSerial SerialBT;
-//int x = IO32;
 
-  // initialize digital pins for Bluetooth Tx and Rx
-  // initialize analog pins for joystick x and y
-
-  //Whatever code we need to use to interface with the Bluetooth module
 void setup() {
-  pinMode(32, INPUT_PULLUP);
-  Serial.begin(115200);
+  pinMode(32, INPUT_PULLUP); //Set pin 32 as pullup for joystick button
+  Serial.begin(115200); //Open serial port at 115200 baud for Bluetooth transmission
   SerialBT.begin("ESP32test"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
   if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+    Serial.write(SerialBT.read()); //Write data recieved over bluetooth to UART
     return;
   }
   String total = "";  
-  int x = analogRead(34); 
-  int y = analogRead(35);
-  int b = digitalRead(32);
+  int x = analogRead(34); //Initialize pin 34 for analog read of joystick x potentiometer
+  int y = analogRead(35); //Initialize pin 35 for analog read of joystick y potentiometer
+  int b = digitalRead(32); //Initialize pin 32 for digital read of joystick button.
+
+//Concatenate output
   total = total + format(x);
   total = total + format(y);
   total = total + b;
   char basicString[9] = {};
   total.toCharArray(basicString, 10);
   for (int i = 0; i <= 8; i++) {
-    SerialBT.write(basicString[i]);
+    SerialBT.write(basicString[i]); //Write output to Bluetooth serial port
   }
   SerialBT.write('\n');
-  //SerialBT.write(basicString);
-  delay(100);
+  delay(100); //100ms delay to make output more human-readable. Will be removed or reduced eventually to reduce latency.
 }
 
 
