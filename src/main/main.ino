471b082f1c4fe6cfc20586d6396ab9713cf8ec72
Avraham Hollander <anhollander516@gmail.com>
2021-12-09 14:44:30 -0500

    Remove lines concerning using make to check whether Bluetooth is enabled - we are using Arduino IDE

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,9 +1,5 @@
 #include "BluetoothSerial.h"
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
 BluetoothSerial SerialBT;
 
 void setup() {
