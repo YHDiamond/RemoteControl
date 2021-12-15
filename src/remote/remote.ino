08b7776ca4e7482807c2533d6423bedae7fb611f
YHDiamond <47502993+yehudahrrs@users.noreply.github.com>
2021-12-15 13:23:22 -0500

    change name of car remote

--- src/remote/remote.ino
+++ src/remote/remote.ino
@@ -5,7 +5,7 @@ BluetoothSerial SerialBT;
 void setup() {
   pinMode(32, INPUT_PULLUP); //Set pin 32 as pullup for joystick button
   Serial.begin(115200); //Open serial port at 115200 baud for Bluetooth transmission
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  SerialBT.begin("CarRemote"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
