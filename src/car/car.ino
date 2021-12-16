e86ddbe3deee2d98712b3000d43eb25630a008dd
Avraham Hollander <anhollander516@gmail.com>
2021-12-16 13:53:02 -0500

    We are now using an ESP32 because the UNO gave us too much trouble

--- src/car/car.ino
+++ src/car/car.ino
@@ -1,49 +1,35 @@
-// Serial communication with Bluetooth HM-10
-// Uses serial monitor for communication with Bluetooth HM-10
-//
-//  Arduino to HM-10 connections
-//  Arduino pin 2 (TX) to voltage divider then to HM-10 RX
-//  Arduino pin 3 to HM-10 TX
-//  Connect GND from the Arduiono to GND on the HM-10
-//
-// When a command is entered in to the serial monitor on the computer 
-// the Arduino will relay the command to the HM-10
+#include "BluetoothSerial.h"
 
+BluetoothSerial SerialBT;
 
-// Library to make a Software UART
-#include <SoftwareSerial.h>
-
-#define RX 2
-#define TX 3
-
-#define BAUDRATE 9600
-
-char c = ' ';
-boolean new_line = true;
-
-// Instantiation of a Software UART
-SoftwareSerial HM10(RX, TX); // (RX, TX)
-
-void setup() {  
-
-  // Start Serial Monitor for feedback
-  Serial.begin(BAUDRATE);
-  
-  // HM-10 default speed in AT command mode
-  HM10.begin(BAUDRATE);
-  Serial.println("HM10 serial started at 9600");
-  
-  Serial.println("Enter AT commands:");
+void setup() {
+  Serial.begin(115200); //Open serial port at 115200 baud for Bluetooth transmission
+  SerialBT.begin("Car"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  HM10.listen();
-  while (HM10.available() > 0) {
-    char c = HM10.read();
-    Serial.println(c);
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read()); //Write data recieved over bluetooth to UART - for debugging purposes; in the final product the car will have no need to communicate back.
+    return;
   }
-  if (Serial.available()) {
-    delay(10);
-    HM10.write(Serial.read());
+    //char c = 'A';
+    //SerialBT.write(c); //Write output to Bluetooth serial port
+    //delay(100); //100ms delay to make output more human-readable. Will be removed or reduced eventually to reduce latency.
+}
+
+
+String format(int i) {
+  String base = String(i);
+  switch (base.length()) {
+    case 1:
+      return String("000") + base;
+    case 2: 
+      return String("00") + base;
+    case 3: 
+      return String("0") + base;
+    default:
+      return base;
   }
+ 
 }
