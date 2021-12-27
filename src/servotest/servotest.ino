101ff4fc9a951b860d76ba7e09cdb8d15d650025
Avraham Hollander <anhollander516@gmail.com>
2021-12-27 17:17:11 -0500

    failed attempts to even get the servo to do anything on its own

--- src/servotest/servotest.ino
+++ src/servotest/servotest.ino
@@ -2,20 +2,25 @@
 
 #define servoPin 33
 Servo servo;
+int pos = 0;
 
 void setup() {
-    //pinMode(34, OUTPUT);
-    
-    servo.attach(servoPin);
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+    servo.setPeriodHertz(50);
+    servo.attach(servoPin, 500, 2400);
 }
 
 void loop() {
-    servo.write(0);
-    delay(1000);
-    servo.write(90);
-    delay(1000);
-    servo.write(180);
-    delay(1000);
-    servo.write(90);
-    delay(1000);
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo.write(pos);    // tell servo to go to position in variable 'pos'
+    delay(15);             // waits 15ms for the servo to reach the position
+    }
+    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+      servo.write(pos);    // tell servo to go to position in variable 'pos'
+      delay(15);             // waits 15ms for the servo to reach the position
+    }
 }
